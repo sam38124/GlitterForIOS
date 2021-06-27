a535a7f11276844555db24e349cddd9e140f5c56
sam38124 <sam38124@gmail.com>
2021-06-28 07:39:14 +0800

    32

--- Sources/Glitter_IOS/GlitterFunction.swift
+++ Sources/Glitter_IOS/GlitterFunction.swift
@@ -12,21 +12,31 @@ class GlitterFunction {
         //取得紀錄
         glitterAct.addJavacScriptInterFace(interface:JavaScriptInterFace(functionName: "getPro", function: {
             item in
+            DispatchQueue.main.async {
             let preferences = UserDefaults.standard
             let data=item.receiveValue["name"]
             let currentLevel = preferences.string(forKey: "\(data!)")
             item.responseValue["data"]=currentLevel
+            print("getPro:\(data!)-\(currentLevel)")
             item.finish()
+            }
         }))
         //存紀錄
         glitterAct.addJavacScriptInterFace(interface:JavaScriptInterFace(functionName: "setPro", function: {
             item in
-            let preferences = UserDefaults.standard
-            let name=item.receiveValue["name"]
-            let data=item.receiveValue["data"]
-            preferences.set(name,forKey: "\(data!)")
-            item.responseValue["result"]=true
-            item.finish()
+            DispatchQueue.main.async {
+                let preferences = UserDefaults.standard
+                let name=item.receiveValue["name"]
+                let data=item.receiveValue["data"]
+                preferences.set("\(data!)",forKey:"\(name!)" )
+                let didSave = preferences.synchronize()
+                if !didSave {
+                    print("saverror")
+                }
+                print("setPro:\(didSave)")
+                item.responseValue["result"]=true
+                item.finish()
+            }
         }))
     }
 }
