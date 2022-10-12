caac89187705c6203bdf32a2361d326d630f04d6
sam38124 <sam38124@gmail.com>
2022-10-12 15:29:00 +0800

    232

--- Sources/Glitter_IOS/extension.swift
+++ Sources/Glitter_IOS/extension.swift
@@ -63,41 +63,6 @@ extension Data {
         return UInt16((crc ^ 0) & 0xFFFF)
     }
 }
-extension UIView {
-    @IBInspectable var shadow: CGSize {
-        get {
-            return layer.shadowOffset
-        }
-        set {
-            layer.shadowOffset = newValue
-            layer.shadowOpacity = 0.5
-            layer.shadowRadius = 0.5
-            layer.shadowColor = UIColor.black.cgColor
-        }}
-    @IBInspectable var cornerRadius: CGFloat {
-        get {
-            return layer.cornerRadius
-        }
-        set {
-            layer.cornerRadius = newValue
-            layer.masksToBounds = newValue > 0
-        }}
-    
-    @IBInspectable var borderWidth: CGFloat {
-        get {
-            return layer.borderWidth
-        }
-        set {
-            layer.borderWidth = newValue
-        }}
-    
-    @IBInspectable var borderColor: UIColor? {
-        get {
-            return UIColor(cgColor: layer.borderColor!)
-        }
-        set {
-            layer.borderColor = newValue?.cgColor
-        }}}
 extension String {
     func getUrls()->[NSURL]{
         let text=(self)
